0012fd83ca3bd487e2528a11ee8ccfb5aae9ca3b
slachapelle <sebastien.lachapelle@live.com>
2018-04-24 15:12:07 -0400

    Add misc utilities

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import argparse
 
 import numpy as np
 from torch import optim
+from torch.autograd import Variable
+from torch.nn import functional as F
 
 from model import *
 from train import TrainLoop
@@ -29,6 +31,8 @@ def experiment(args, reset_epoch=False):
                             args.exp_folder,
                             args.dataset,
                             resume=True)
+
+    print args.exp_folder
 
     # model
     model = eval(hyper['MODEL_CLASS']+'(hyper)')
@@ -61,18 +65,28 @@ def experiment(args, reset_epoch=False):
 def restartExperiment(args):
     experiment(args, reset_epoch=True )
 
+def seedExperiment(args):
+    """ Produce a hyper.pkl with the desired hparams """
+    getDefaultHyper(args.model_class,
+                    args.exp_folder,
+                    args.dataset,
+                    resume=True)
+
 def evaluate(args):
     hyper = getDefaultHyper(args.model_class,
                             args.exp_folder,
                             args.dataset,
                             resume=True)
+    hyper['shuffle'] = False
 
     # model
     model = eval(hyper['MODEL_CLASS']+'(hyper)')
 
     # data
     dataloaders = data.getLoaders(hyper,
-                                  data_modes=['train','valid'])
+                                  data_modes=['train','valid'],
+                                  data_augment=False)
+
     print 'train set has ', len(dataloaders['train'].dataset), 'examples'
     print 'valid set has ', len(dataloaders['valid'].dataset), 'examples'
     # optimizer
@@ -91,6 +105,72 @@ def evaluate(args):
     if isinstance(model, GradientDescentPredictor):
         which += ['sigma_2']
     postTrain.graph(which=which)
+
+    for dat in dataloaders['valid']:
+        x = Variable(dat['input'].cuda()) 
+        y = Variable(dat['label'].cuda())
+        break
+
+    y_pred = model(x)
+
+    if isinstance(model, ConvInit):
+        y_pred = F.sigmoid(y_pred)
+    else:
+        # 25th appears to be interesting visually...
+        vizDenoise(x[25].data.cpu().numpy(),
+                   y_pred[:,25,:,:,:].data.cpu().numpy(),
+                   y[25].data.cpu().numpy(),
+                   hyper['EXPERIMENT_FOLDER'])
+        y_pred = y_pred[-1]
+
+    visualizePredictions(x.data.cpu().numpy(), 
+                         y_pred.data.cpu().numpy(),
+                         y.data.cpu().numpy(),
+                         hyper['EXPERIMENT_FOLDER'],
+                         name='viz_Final', 
+                         nb_ex=5, 
+                         seed=56063)
+
+def showData(args):
+
+    hyper = {}
+    hyper['shuffle'] = False
+    hyper['norm_data'] = False
+    hyper['bs'] = 32
+    hyper['DATASET'] = args.dataset
+    
+    # data
+    dataloaders = data.getLoaders(hyper,
+                                  data_modes=['train','valid'],
+                                  data_augment=False)
+
+    print dataloaders
+
+    k = 0 
+    for dat in dataloaders['valid']:
+        x = dat['input'].cpu().numpy()
+        y = dat['label'].cpu().numpy()
+        if k == 3:
+            break
+        else:
+            k += 1
+
+    fig = plt.figure(figsize=(10,10))
+    plt.title('Noisy image, denoised image and ground truth image')
+    plt.axis('off')
+    for i in range(3):
+        for j in range(5):
+
+            ax1 = fig.add_subplot(5,3,i+j*3+1)
+            ax1.imshow(x[3*i + 5*j].reshape(-1, y.shape[-1]))
+            ax1.axis('off')
+            ax1.set_xticklabels([])
+            ax1.set_yticklabels([])
+
+    plt.subplots_adjust(wspace=0.1, hspace=0.1)
+    plt.savefig('/u/lachaseb/SPEN/images.jpg')
+
+    plt.close(fig)
 
 def trainVStest(args):
 
@@ -130,7 +210,7 @@ if __name__ == '__main__':
         data.setupData(args.dataset, local_folder=LOCAL_FOLDER)
 
     if not os.path.exists(args.exp_folder):
-    	os.makedirs(args.exp_folder)
+        os.makedirs(args.exp_folder)
 
     # Run a function from main.py
     f = eval(args.mode)
