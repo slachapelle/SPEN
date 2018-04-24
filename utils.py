593cfb0374ae814e79aafba1f8d478bcb9344a24
slachapelle <sebastien.lachapelle@live.com>
2018-04-24 15:08:48 -0400

    Add visualisation utility

--- utils.py
+++ utils.py
@@ -6,9 +6,10 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-def visualizePredictions(x, y_pred, y_gt, folder, name='viz_pred', nb_ex=5):
+def visualizePredictions(x, y_pred, y_gt, folder, name='viz_pred', nb_ex=5, seed=123):
     """Produces visualization of the predictions"""
 
+    np.random.seed(seed)
     rnd_idx = np.array(range(x.shape[0]))
     np.random.shuffle(rnd_idx)
 
@@ -41,6 +42,41 @@ def visualizePredictions(x, y_pred, y_gt, folder, name='viz_pred', nb_ex=5):
     plt.savefig(folder+'/'+name+'.jpg')
 
     plt.close(fig)
+
+def vizDenoise(x,y_seq,y,folder):
+
+    fig = plt.figure(figsize=(12,3))
+    plt.title('Noisy image, denoising process and ground truth image')
+    plt.axis('off')
+
+
+    print x.shape, y_seq.shape, y.shape
+
+    ax1 = fig.add_subplot(1,2+y_seq.shape[0],1)
+    ax1.imshow(x.reshape(-1, y.shape[-1]))
+    ax1.axis('off')
+    ax1.set_xticklabels([])
+    ax1.set_yticklabels([])
+
+    for i in range(y_seq.shape[0]):
+
+        ax1 = fig.add_subplot(1,2+y_seq.shape[0],i+2)
+        ax1.imshow(y_seq[i].reshape(-1, y.shape[-1]))
+        ax1.axis('off')
+        ax1.set_xticklabels([])
+        ax1.set_yticklabels([])
+
+    ax1 = fig.add_subplot(1,2+y_seq.shape[0],i+3)
+    ax1.imshow(y.reshape(-1, y.shape[-1]))
+    ax1.axis('off')
+    ax1.set_xticklabels([])
+    ax1.set_yticklabels([])
+
+    plt.subplots_adjust(wspace=0.1, hspace=0.1)
+    plt.savefig(folder+'/vizDenoise.jpg')
+
+    plt.close(fig)
+
 
 def finishing(experiment_folder):
     """Let a marker in the experiment_folder.
